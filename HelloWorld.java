d272a5c17716e3dc574fd4b7b2e2ea71be06ea8a
egle <fluxus_egle@yahoo.com>
2017-03-16 19:08:07 +0200

    Add other Input class functionality

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * @author zwaclaw
  *
@@ -8,5 +8,30 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO UGLY WORLD!");
 	System.out.println("The World is not ugly!");
+
+
+Scanner reader = new Scanner(System.in);
+		
+		String string;
+		int sk;
+		String vardas;
+
+
+System.out.println("Please enter string:");
+		string = reader.next();
+
+System.out.println("Please enter sk:");
+		sk = reader.nextInt();
+		
+		System.out.println(string + " " + sk);
+
+
+System.out.println("Please enter your name:");
+		vardas = reader.next();
+		
+		System.out.println("Hello " + vardas);
+
+	
+
 }
 }
